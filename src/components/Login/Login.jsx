87fc2d497b84ec73e643c2730ac302ea92771043
VladimirNeon <127400180+VladimirNeon@users.noreply.github.com>
2024-12-11 22:37:41 +0200

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -15,7 +15,7 @@ const LoginPage = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch("http://localhost:8080/api/auth/login", {
+            const response = await fetch("https://caloriecalculator-b-production.up.railway.app/api/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
