e436e04b258dc3d575017b048dcfc0e62275c4ae
VladimirNeon <127400180+VladimirNeon@users.noreply.github.com>
2024-12-11 22:07:34 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://caloriecalculator-b-production.up.railway.app";
 
 // Отримати токен із localStorage
 const getToken = () => localStorage.getItem("token");
